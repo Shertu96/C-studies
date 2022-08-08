77eaf5c60ba48df3deed82ffc725d91a34bfc760
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-08-08 15:20:57 +0300

    Если особенных пользователей несколько

---
+++ FavouriteUsers/Program.cs
@@ -0,0 +1,13 @@
+﻿//Для нескольких особенных пользователей
+
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username == "Маша" || username == "Илья" || username == "Анна")
+{
+    Console.WriteLine("Ура, это же особенный пользователь " + username + "!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
