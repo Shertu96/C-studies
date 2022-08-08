bee79fa3bfd2be767210f50776b6135d4ef49998
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-08-08 20:07:20 +0300

    Задача 2. Сравнить 2 числа, которые вводит пользователь.

---
+++ HW2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Сравниваем 2 числа, которые вводит пользователь:
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число");
+int userNumberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int userNumberB = Convert.ToInt32(Console.ReadLine());
+
+int max = userNumberA;
+
+if(userNumberA > max) max = userNumberA;
+if(userNumberB > max) max = userNumberB;
+
+Console.WriteLine(max);
