4b9b61c923982e6817e4471cbe134ea5b26b42c9
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-08-08 20:53:41 +0300

    Задача 6. Проверить, чётное ли число ввёл пользователь

---
+++ HW6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Проверить, чётное ли число ввёл пользователь
+
+Console.WriteLine("Введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if(userNumber % 2 == 0) //проверка, делится ли число x нацело на число y - (x % y == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
