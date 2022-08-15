e05b204ac0c6a493a5ce4dad3aa397fa5557f65e
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-08-15 23:58:58 +0300

    Fixed Notifications

--- FavouriteUsers/Program.cs
+++ FavouriteUsers/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Для нескольких особенных пользователей
 
 Console.WriteLine("Введите имя пользователя: ");
-string username = Console.ReadLine();
+string? username = Console.ReadLine();
 if(username == "Маша" || username == "Илья" || username == "Анна")
 {
     Console.WriteLine("Ура, это же особенный пользователь " + username + "!");
