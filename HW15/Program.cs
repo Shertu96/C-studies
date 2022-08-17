7f9bff95705891346503baf45fdc8998b400b807
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-08-17 15:01:25 +0300

    Task 15 done. Tasks 10, 13 checked, reviewed

---
+++ HW15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Принять на вход цифру, обозначающую день недели, и проверить, выходной ли это день.
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите день недели (цифру от 1 до 7): ");
+        string? day = Console.ReadLine();
+        Console.WriteLine(usersDay(day));
+    }
+
+    static string usersDay(string? day)
+    {
+        if (day == "6" || day == "7")
+        {
+            return "День выходной";
+        }
+        return "День будний";
+    }
+}
