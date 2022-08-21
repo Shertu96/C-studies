1db48b6ef33378fe76b0436052bfd62a9d7c540a
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-08-21 21:04:07 +0300

    Tasks 19, 21 done

---
+++ HW19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Принять пятизначное число и проверить, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+string? userNumber = Console.ReadLine();
+
+if(userNumber!.Length == 5)
+{
+    Check();
+}
+
+else
+{
+    Console.WriteLine("Число должно состоять из пяти знаков: ");
+}
+
+
+    void Check()
+    {
+        if(userNumber[0] == userNumber[4] && userNumber[1] == userNumber[3])
+        {
+            Console.WriteLine("Число - палиндром!");
+        }
+    
+        else
+        {
+            Console. WriteLine("Число не является палиндромом!");
+        }
+    }
---
+++ HW21/Program.cs
@@ -0,0 +1,27 @@
+﻿// Принять на вход координаты двух точек и найти расстояние между ними в 3D пространстве.
+
+
+int Coordinate(string coorName, string positionName)
+{
+    Console.Write("Введите координату " + coorName + " точки: " + positionName);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+
+int m = (x2 - x1);
+int n = (y2 - y1);
+int p = (z2 - z1);
+
+double d = Math.Sqrt((Math.Pow(m,2)) + 
+                (Math.Pow(n,2)) +  
+                (Math.Pow(p,2)));
+
+Console.WriteLine("Расстояние между точками равно: ");
+Console.WriteLine(Math.Round(d, 2));
