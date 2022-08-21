ba79c742457ac2c9675b6b6bdee33ee0b0e81211
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-08-21 21:28:58 +0300

    Task 23 done

---
+++ HW23/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Принять на вход число (N) и выдать таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = 0;
+
+for (int N = Convert.ToInt32(Console.ReadLine()); number < N; number++)
+{
+    Console.WriteLine(Math.Pow(number,3));
+}
+
