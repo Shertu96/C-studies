9a6b65b709cd88a9eceee784d221356e2591ceb1
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-08-29 18:55:49 +0300

    Fibonacci algorythm reviewed

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -2,17 +2,24 @@
 
 int Fibonacci(int n)
 {
+    /*
+    if(n == 0)
+    {
+        return 0;
+    }
     if(n == 1 || n == 2)
     {
         return 1;
     }
-    else
+    */
+    if (n == 0 || n == 1)
     {
-        return Fibonacci(n - 1) + Fibonacci(n - 2);
+        return n;
     }
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
-for(int i = 1; i < 17; i++) // Число можно задать любое, после 40 начнет работать медленно из-за зтрат ресурсов
+for(int i = 0; i < 17; i++) // Число можно задать любое, после 40 начнет работать медленно из-за затрат ресурсов
 {
     Console.WriteLine(Fibonacci(i));
 }
