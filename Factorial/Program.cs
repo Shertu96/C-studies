90ecff89ba26d8ae1a258d9d89aac0b2961cf1a8
Евгения <Evgenia@MacBook-Pro-Evgenia.local>
2022-08-29 15:19:17 +0300

    Работа с двумерными массивами. Рекурсия, факториал, числа Фибоначчи

---
+++ Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿// Факториал через рекурсию
+
+double Factorial (int n) // Тип int переполянется на 17, надо заменить на double 
+{
+    if(n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
+
+Console.WriteLine(Factorial(40));
+
+for(int i = 1; i < 40; i++) // Проверка работы алгоритма, вывод всех чисел ДО искомого
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿// Числа Фибоначчи
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
+
+for(int i = 1; i < 17; i++) // Число можно задать любое, после 40 начнет работать медленно из-за зтрат ресурсов
+{
+    Console.WriteLine(Fibonacci(i));
+}
